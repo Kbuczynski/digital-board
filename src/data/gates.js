bd8f07f175d2e96e2bb1c7c640e57a860d8140a5
Kbuczynski <kamil.buczynski00@interia.pl>
2021-06-03 16:51:22 +0200

    created inputs and diods

--- src/data/gates.js
+++ src/data/gates.js
@@ -3,35 +3,51 @@ export const GATES = [
         name: "NOT",
         operation: (a) => !a,
         symbol: "NOT.svg",
+        inputs: 10,
     },
     {
         name: "OR",
         operation: (a, b) => a | b,
         symbol: "OR.svg",
+        inputs: 2,
     },
     {
         name: "NOR",
         operation: (a, b) => !(a | b),
         symbol: "NOR.svg",
+        inputs: 2,
     },
     {
         name: "AND",
         operation: (a, b) => a & b,
         symbol: "AND.svg",
+        inputs: 2,
     },
     {
         name: "NAND",
         operation: (a, b) => !(a & b),
         symbol: "NAND.svg",
+        inputs: 2,
     },
     {
         name: "XOR",
         operation: (a, b) => a ^ b,
         symbol: "XOR.svg",
+        inputs: 2,
     },
     {
         name: "XNOR",
         operation: (a, b) => !(a ^ b),
         symbol: "XNOR.svg",
+        inputs: 2,
     },
+    {
+        name: "DIODE",
+        symbol: "DIODE.svg",
+        inputs: 1,
+    },
+    {
+        name: "INPUT",
+        symbol: "INPUT.svg",
+    }
 ]
